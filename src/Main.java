218297a08afb8e3ef0e9d73c09df7c3e73efa9f7
Aitor Sanchez Cervantes <aitor614@gmail.com>
2022-09-01 22:53:11 +0200

    Crear gets y sets de cada propiedad

--- src/Main.java
+++ src/Main.java
@@ -7,4 +7,23 @@ class Persona {
     private int edad;
     private String nombre;
     private int telefonoPersona;
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    public int getEdad() {
+        return this.edad;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getNombre() {
+        return this.nombre;
+    }
+    public void setTelefonoPersona(int telefonoPersona) {
+        this.telefonoPersona = telefonoPersona;
+    }
+    public int getTelefonoPersona() {
+        return this.telefonoPersona = telefonoPersona;
+    }
 }
